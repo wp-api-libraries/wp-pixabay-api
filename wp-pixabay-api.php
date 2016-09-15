ee2911a692c2d51b0ec92402effe51f130bf7239
Brandon Hubbard <bkhubbard@gmail.com>
2016-09-15 15:48:43 -0700

    Getting Started

---
+++ wp-pixabay-api.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * WP Pixabay API (https://pixabay.com/api/docs/)
+ *
+ * @package WP-Pixabay-API
+ */
+
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+/* Check if class exists. */
+if ( ! class_exists( 'PixabayAPI' ) ) {
+
+
+	/**
+	 * PixabayAPI class.
+	 */
+	class PixabayAPI {
+
+	}
+
+}
